243951e8881fd93ff4e48d91f57e823e975f6f48
Blizzard-4 <blizzard-4@Admin-4s-iMac.local>
2023-08-04 14:39:56 +0800

    add:worldfind_app

--- wordfind_app/lib/start_page.dart
+++ wordfind_app/lib/start_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wordfind_app/gradient_text.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({super.key});
@@ -41,8 +42,49 @@ class StartPage extends StatelessWidget {
               Padding(
                 padding: EdgeInsets.only(top: 20),
               ),
+              GradientText('Player Name', 20.0),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+              ),
             ],
           ),
+        ),
+      ),
+      floatingActionButton: StartButton('Start'),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class StartButton extends StatelessWidget {
+  const StartButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 310,
+      height: 60,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          colors: [Color(0xFFE86b02), Color(0xFFFA9541)],
+        ),
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+        child: Text(
+          'START',
+          style: TextStyle(
+              fontFamily: 'Nunito', fontSize: 24, fontWeight: FontWeight.w700),
         ),
       ),
     );
